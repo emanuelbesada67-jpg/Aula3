9903d8fd6421caa835ce2ed8d53eefab86681555
EMANUEL AMEIXOEIRA BESADA <emanuel.besada@aluno.senac.rio>
2026-04-10 21:22:48 -0300

    aula html

---
+++ js/script.js
@@ -0,0 +1,8 @@
+//função para verificar idade 
+function verificaridade(){
+    let idade = parseInt(document.getElementById("idade").value);
+    getElementById9(("idade").value);
+    if (idade < 18) {
+        alert("para viajar sozinho, é necessario ser maior de idade");
+    }
+}
