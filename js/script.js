566250917341e9e93cd1e8b41fb3be491143e4ee
EMANUEL AMEIXOEIRA BESADA <emanuel.besada@aluno.senac.rio>
2026-04-15 20:36:58 -0300

    adicionar backend

--- js/script.js
+++ js/script.js
@@ -6,7 +6,44 @@ function verificaridade() {
         alert("para viajar sozinho, é necessario ser maior de idade");
     }
 }
+// função para a partir do cep, buscar o endereço do usuario.
+async function buscarCep(){
+     //pegar o valor digitado no input do CEP
+     let cep = document.getElementById("cep").value;
+     cep = cep.replace(/\D/g, "");
 
+     //validar se o CEP tem 8 numeros
+     if(cep.length !== 8){
+        document.getElementById("mensagem").textContent =
+        "Cep inválido";
+        return;
+     }
+
+     try{
+        //faz a requisição para a api do viaCEP
+        let resposta = await fetch('https://viacep.com.br/ws/${cep}/json/');
+
+        let dados = await resposta.json();
+
+        //verificar se o cep não foi encontrado
+
+        if(dados.erro){
+            document.getElementById("mensagem").textContent ="cep não encontrado";
+        }
+
+        //preencher os campos do formulario:
+
+        document.getElementById("logradouro").value = dados.logradouro;
+        document.getElementById("Bairro").value = dados.bairro;
+        document.getElementById("cidade").value = dados.localidade;
+        document.getElementById("UF").value = dados.UF;
+
+        document.getElementById("mensagem").textContent ="endereço encontrado com sucesso";
+    }catch (erro){
+        document.getElementById("mensagem").textContent = "erro ao buscar cep"
+    }
+
+}
 function enviarFormulario(event) {
     //a linha abaixo impede o recarregamento da pagina
     event.preventDefault();
